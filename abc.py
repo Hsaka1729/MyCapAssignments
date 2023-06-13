2050cf6dafd00aea1e861ce0c9a7d2efd9f7e3a6
u2205010@rajagiri.edu.in <135734121+Hsaka1729@users.noreply.github.com>
2023-06-13 22:28:35 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,10 @@
+import math
+
+# Prompt the user to enter the radius
+radius = float(input("Input the radius of the circle: "))
+
+# Calculate the area
+area = math.pi * radius**2
+
+# Print the result
+print("The area of the circle with radius", radius, "is:", area)
