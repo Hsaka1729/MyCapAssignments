96c602ad08ab3b31cfd2eb3deeb72b74735dbe99
u2205010@rajagiri.edu.in <135734121+Hsaka1729@users.noreply.github.com>
2023-06-16 21:27:37 +0530

    Add files via upload
    program for extension

---
+++ extension.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 21:24:51 2023
+
+@author: AKASH P.K
+"""
+
+filename = input("Input the Filename: ")
+extension = filename.split(".")[-1]
+
+print("The extension of the file is:", extension)
